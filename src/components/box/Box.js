4d4c49cbd5153c5c90f04b238d48b6f5332587d7
kevinnail <fumalicious@gmail.com>
2023-01-30 14:26:28 -0800

    removed space in app.css

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -5,7 +5,15 @@ import { useContext } from 'react';
 
 export default function Box({ box }) {
   const { handleClick, active } = useContext(GameContext);
+  const playedBox = document.getElementById(box.space);
 
+  if (!active) {
+    playedBox.classList.remove('box');
+    playedBox.classList.add('played');
+  } else {
+    playedBox.classList.remove('played');
+    playedBox.classList.add('box');
+  }
   return (
     <div id={box.space} onClick={() => handleClick(box)} className="box">
       {box.context}
